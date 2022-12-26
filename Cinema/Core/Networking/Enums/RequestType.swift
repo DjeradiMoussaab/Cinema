8b90162e3d6696cc62aad489a79c9d7be160571f
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-12-26 10:44:43 +0100

    Create Request Type Enum (RequestType.swift)

---
+++ Cinema/Core/Networking/Enums/RequestType.swift
@@ -0,0 +1,16 @@
+//
+//  RequestType.swift
+//  Cinema
+//
+//  Created by Moussaab Djeradi on 26/12/2022.
+//
+
+import Foundation
+
+enum RequestType: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case PATCH
+}
