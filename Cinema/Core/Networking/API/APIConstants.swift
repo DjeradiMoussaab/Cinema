c33a99836e28ceeacaa9a4a8a4469c7e7aa5cd89
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-12-26 10:47:36 +0100

    Create the API Constants (APIConstants.swift)

---
+++ Cinema/Core/Networking/API/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  Cinema
+//
+//  Created by Moussaab Djeradi on 26/12/2022.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let host = "api.themoviedb.org/3"
+    static let scheme = "https"
+}
+
+extension APIConstants {
+    static let accessToken = "fb4da7e70c1614ad65b4a521264d9459"
+}
