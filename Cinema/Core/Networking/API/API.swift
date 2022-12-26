6a4526a943fe2e296b909199af62ec906ac51abd
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-12-26 10:57:52 +0100

    Create the API Protocol and API final class (API.swift)

---
+++ Cinema/Core/Networking/API/API.swift
@@ -0,0 +1,26 @@
+//
+//  API.swift
+//  Cinema
+//
+//  Created by Moussaab Djeradi on 26/12/2022.
+//
+
+import Foundation
+
+protocol APIProtocol {
+    func makeRequest(_ endpoint: Endpoint) async throws -> Data
+}
+
+final class API: APIProtocol {
+        
+    func makeRequest(_ endpoint: Endpoint) async throws -> Data {
+        let requestURL = try endpoint.generateRequestURL()
+        let (data, response) = try await URLSession.shared.data(for: requestURL)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw ErrorType.invalideResponse
+        }
+        return data
+    }
+    
+}
