2467df80a5522a5cad86e9d0996e9c4dae926dcc
Moussaab Djeradi <fm_djeradi@esi.dz>
2022-12-30 16:33:09 +0100

    fix small bug.

--- Cinema/AppDelegate/AppDelegate.swift
+++ Cinema/AppDelegate/AppDelegate.swift
@@ -13,11 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
         return true
     }
 
