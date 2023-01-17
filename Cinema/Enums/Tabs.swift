583bd8f632c7e4db957006256330ce4607c14eb8
Moussaab Djeradi <fm_djeradi@esi.dz>
2023-01-17 13:56:36 +0100

    Create the Tabs enum

---
+++ Cinema/Enums/Tabs.swift
@@ -0,0 +1,15 @@
+//
+//  Tabs.swift
+//  Cinema
+//
+//  Created by Moussaab Djeradi on 17/1/2023.
+//
+
+import Foundation
+
+enum Tabs: String {
+    case trending = "TRENDING"
+    case search = "SEARCH"
+    case favorite = "FAVORITE"
+    case profile = "PROFILE"
+}
