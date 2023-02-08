31a4c60acef69a4a5849f4eaaa8ad24f3d36a510
Moussaab Djeradi <fm_djeradi@esi.dz>
2023-02-08 15:15:46 +0100

    Create the Session Class

---
+++ Cinema/Features/Login/Session.swift
@@ -0,0 +1,71 @@
+//
+//  Session.swift
+//  Cinema
+//
+//  Created by Moussaab Djeradi on 24/1/2023.
+//
+
+import Foundation
+
+enum UserKeys: String {
+    case username = "username"
+    case name = "name"
+    case id = "id"
+    case hash = "hash"
+    case iso3166_1 = "iso3166_1"
+    case iso639_1 = "iso639_1"
+    case includeAdult = "includeAdult"
+    
+    case isUserConnected = "isUserConnected"
+}
+
+class Session {
+    
+    fileprivate static let defaults = UserDefaults.standard
+    
+    private var user: User!
+            
+    static func setUser(_ user: User) {
+        defaults.setValue(user.username, forKey: UserKeys.username.rawValue)
+        defaults.setValue(user.name, forKey: UserKeys.name.rawValue)
+        defaults.setValue(user.id, forKey: UserKeys.id.rawValue)
+        defaults.setValue(user.avatar.gravatar.hash, forKey: UserKeys.hash.rawValue)
+        defaults.setValue(user.iso3166_1, forKey: UserKeys.iso3166_1.rawValue)
+        defaults.setValue(user.iso639_1, forKey: UserKeys.iso639_1.rawValue)
+        defaults.setValue(user.includeAdult, forKey: UserKeys.includeAdult.rawValue)
+        
+        defaults.setValue(true, forKey: UserKeys.isUserConnected.rawValue)
+    }
+    
+    static func getUser() -> User? {
+        
+        if let hash = defaults.string(forKey: UserKeys.hash.rawValue),
+           let iso639_1 = defaults.string(forKey: UserKeys.iso639_1.rawValue),
+           let iso3166_1 = defaults.string(forKey: UserKeys.iso3166_1.rawValue),
+           let name = defaults.string(forKey: UserKeys.name.rawValue),
+           let username = defaults.string(forKey: UserKeys.username.rawValue)
+        {
+            let includeAdult = defaults.bool(forKey: UserKeys.includeAdult.rawValue)
+            let id = defaults.integer(forKey: UserKeys.id.rawValue)
+            
+            let user = User(
+                avatar: Avatar(gravatar: Gravatar(hash: hash)),
+                id: id,
+                iso639_1: iso639_1,
+                iso3166_1: iso3166_1,
+                name: name,
+                includeAdult: includeAdult,
+                username: username)
+            return user
+        }
+        return nil
+    }
+    
+    static func isUserConnected() -> Bool {
+        return defaults.bool(forKey: UserKeys.isUserConnected.rawValue)
+    }
+    
+    static func userDeconnected() {
+        defaults.setValue(false, forKey: UserKeys.isUserConnected.rawValue)
+    }
+}
