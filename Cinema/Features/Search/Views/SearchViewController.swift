3d235b8d263aecb3a8548d80059f0fd60997d1a4
Moussaab Djeradi <fm_djeradi@esi.dz>
2023-05-03 10:21:08 +0100

    refactoring/enhancing the search page UI

--- Cinema/Features/Search/Views/SearchViewController.swift
+++ Cinema/Features/Search/Views/SearchViewController.swift
@@ -20,17 +20,6 @@ class SearchViewController: UIViewController {
     
     var tableView: UITableView!
     
-    /*private lazy var dataSource = RxTableViewSectionedReloadDataSource<SearchItemSection>(configureCell: { datasource, tableView, indexPath, item in
-        print("idddd : \(SearchItemCell.reuseIdentifier)")
-        
-        let searchItemCell = tableView.dequeueReusableCell(withIdentifier: SearchItemCell.reuseIdentifier, for: indexPath) as! SearchItemCell
-        self.searchViewModel.downloadImage(for: item)
-            .bind(to: searchItemCell.searchImageView.rx.image)
-            .disposed(by: self.disposeBag)
-        searchItemCell.configure(with: item)
-        return searchItemCell
-    })*/
-    
     private lazy var dataSource = RxTableViewSectionedReloadDataSource<SearchItemSection>(configureCell: { datasource, tableView, indexPath, item in
         let searchItemCell = tableView.dequeueReusableCell(withIdentifier: SearchItemCell.reuseIdentifier, for: indexPath) as! SearchItemCell
 
@@ -45,19 +34,15 @@ class SearchViewController: UIViewController {
         return searchItemCell
     })
     
-    let searchTextField : UITextField = {
-        let v = UITextField()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.placeholder = "SEARCHING..."
-        v.layer.borderWidth = 1.0
-        v.layer.cornerRadius = 10.0
-        v.textAlignment = .center
-        v.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        return v
-    }()
-    
     override func viewDidLoad() {
-        self.searchViewModel = SearchViewModel(searchInput: searchTextField.rx.text.orEmpty.asDriver())
+        
+        let searchController = UISearchController()
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.obscuresBackgroundDuringPresentation = false // The default is true.
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false // Make the search bar always visible.
+        
+        self.searchViewModel = SearchViewModel(searchInput: (navigationItem.searchController?.searchBar.rx.text.orEmpty.asDriver())!)
         setupViews()
         setupBinding()
     }
@@ -65,6 +50,7 @@ class SearchViewController: UIViewController {
     private func setupViews() {
         view.backgroundColor = .systemGroupedBackground
         self.title = Tabs.search.rawValue
+        
         tableView = UITableView(frame: self.view.frame, style: .insetGrouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .systemGroupedBackground
@@ -73,17 +59,10 @@ class SearchViewController: UIViewController {
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 96, bottom: 0, right: 0)
         tableView.register(SearchItemCell.self, forCellReuseIdentifier: SearchItemCell.reuseIdentifier)
         
-        
-        view.addSubview(searchTextField)
         view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            searchTextField.heightAnchor.constraint(equalToConstant: 40),
-            
-            tableView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
